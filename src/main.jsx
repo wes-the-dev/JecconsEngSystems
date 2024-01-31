2ef5e1efd3fecc807243d12782e937c31117133a
Wesley Chigbu <chigbuw@yahoo.com>
2024-01-31 13:10:46 +0100

    hero_layout

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { Homepage } from './pages/homePage'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Homepage />
+  </React.StrictMode>,
+)
