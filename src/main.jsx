2580a538e41a9ed6588351c304aa404dbef8e74e
Wesley Chigbu <chigbuw@yahoo.com>
2024-03-13 21:33:27 +0100

    ..

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { Homepage } from './pages/homePage'
+import { Homepage } from './pages/HomePage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
