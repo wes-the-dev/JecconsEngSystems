d1131b1ac3de2472550f6f134455030e2eb98c41
Wesley Chigbu <chigbuw@yahoo.com>
2024-03-20 23:37:34 +0100

    ...

--- src/components/Homepage/CompanyBrief.jsx
+++ src/components/Homepage/CompanyBrief.jsx
@@ -3,28 +3,28 @@ function CompanyBrief() {
   return (
     <div className="bg-[#F6F6F6] px-[60px] py-20 font-['PP-Telegraf'] text-[var(--primary-red)] flex justify-between max-[770px]:px-5 max-[770px]:py-10">
         <div className="completed-projects-brief flex gap-2 items-center">
-            <span className="text-[56px] max-[1100px]:text-[50px] max-[900px]:text-[40px]">
+            <span className="text-[56px] max-[1100px]:text-[50px] max-[900px]:text-[40px] max-[650px]:text-[30px] max-[550px]:text-[20px]">
                 100+
             </span>
             <div className="flex flex-col gap-0">
-                <span className="text-[20px] max-[1100px]:text-[16px] max-[900px]:text-[14px]">COMPLETED</span>
-                <span className="text-[30px] font-bold max-[1100px]:text-[26px] max-[900px]:text-[20px]">Projects</span>
+                <span className="text-[20px] max-[1100px]:text-[16px] max-[900px]:text-[14px] max-[550px]:text-[12px]">COMPLETED</span>
+                <span className="text-[30px] font-bold max-[1100px]:text-[26px] max-[900px]:text-[20px] max-[650px]:text-[16px] max-[550px]:text-[12px]">Projects</span>
             </div>
         </div>
         <div className="years-experience-hm flex gap-2 items-center">
-            <span className="text-[56px] max-[1100px]:text-[50px] max-[900px]:text-[40px]"> 40  </span>
+            <span className="text-[56px] max-[1100px]:text-[50px] max-[900px]:text-[40px] max-[650px]:text-[30px] max-[550px]:text-[20px]"> 40  </span>
             <div className="flex flex-col gap-0">
-                <span className="text-[20px] max-[1100px]:text-[16px] max-[900px]:text-[14px]">YEARS OF</span>
-                <span className="text-[30px] font-bold max-[1100px]:text-[26px] max-[900px]:text-[20px]">Experience</span>
+                <span className="text-[20px] max-[1100px]:text-[16px] max-[900px]:text-[14px] max-[550px]:text-[12px]">YEARS OF</span>
+                <span className="text-[30px] font-bold max-[1100px]:text-[26px] max-[900px]:text-[20px] max-[650px]:text-[16px] max-[550px]:text-[12px]">Experience</span>
             </div>
         </div>
 
         <div className="cert-professionals flex gap-2 items-center">
-            <span className="text-[56px] max-[1100px]:text-[50px] max-[900px]:text-[40px]">
+            <span className="text-[56px] max-[1100px]:text-[50px] max-[900px]:text-[40px] max-[650px]:text-[30px] max-[550px]:text-[20px]">
                 50+            </span>
             <div className="flex flex-col gap-0">
-                <span className="text-[20px] max-[1100px]:text-[16px] max-[900px]:text-[14px]">CERTIFIED</span>
-                <span className="text-[30px] font-bold max-[1100px]:text-[26px] max-[900px]:text-[20px]">Professionals</span>
+                <span className="text-[20px] max-[1100px]:text-[16px] max-[900px]:text-[14px] max-[550px]:text-[12px]">CERTIFIED</span>
+                <span className="text-[30px] font-bold max-[1100px]:text-[26px] max-[900px]:text-[20px] max-[650px]:text-[16px] max-[550px]:text-[12px]">Professionals</span>
             </div>
         </div>
     </div>
