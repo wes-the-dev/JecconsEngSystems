b44407b1d0a133e4315c3306693c16d0402f2f36
Wesley Chigbu <chigbuw@yahoo.com>
2024-03-20 23:42:33 +0100

    company-brief-mobile

--- src/components/Homepage/CompanyBrief.jsx
+++ src/components/Homepage/CompanyBrief.jsx
@@ -1,9 +1,9 @@
 
 function CompanyBrief() {
   return (
-    <div className="bg-[#F6F6F6] px-[60px] py-20 font-['PP-Telegraf'] text-[var(--primary-red)] flex justify-between max-[770px]:px-5 max-[770px]:py-10">
+    <div className="bg-[#F6F6F6] px-[60px] py-20 font-['PP-Telegraf'] text-[var(--primary-red)] flex justify-between max-[770px]:px-5 max-[380px]:px-3 max-[770px]:py-10 max-[430px]:py-6">
         <div className="completed-projects-brief flex gap-2 items-center">
-            <span className="text-[56px] max-[1100px]:text-[50px] max-[900px]:text-[40px] max-[650px]:text-[30px] max-[550px]:text-[20px]">
+            <span className="text-[56px] max-[1100px]:text-[50px] max-[900px]:text-[40px] max-[650px]:text-[30px] max-[550px]:text-[20px] max-[430px]:text-[16px]">
                 100+
             </span>
             <div className="flex flex-col gap-0">
@@ -12,7 +12,7 @@ function CompanyBrief() {
             </div>
         </div>
         <div className="years-experience-hm flex gap-2 items-center">
-            <span className="text-[56px] max-[1100px]:text-[50px] max-[900px]:text-[40px] max-[650px]:text-[30px] max-[550px]:text-[20px]"> 40  </span>
+            <span className="text-[56px] max-[1100px]:text-[50px] max-[900px]:text-[40px] max-[650px]:text-[30px] max-[550px]:text-[20px] max-[430px]:text-[16px]"> 40  </span>
             <div className="flex flex-col gap-0">
                 <span className="text-[20px] max-[1100px]:text-[16px] max-[900px]:text-[14px] max-[550px]:text-[12px]">YEARS OF</span>
                 <span className="text-[30px] font-bold max-[1100px]:text-[26px] max-[900px]:text-[20px] max-[650px]:text-[16px] max-[550px]:text-[12px]">Experience</span>
@@ -20,7 +20,7 @@ function CompanyBrief() {
         </div>
 
         <div className="cert-professionals flex gap-2 items-center">
-            <span className="text-[56px] max-[1100px]:text-[50px] max-[900px]:text-[40px] max-[650px]:text-[30px] max-[550px]:text-[20px]">
+            <span className="text-[56px] max-[1100px]:text-[50px] max-[900px]:text-[40px] max-[650px]:text-[30px] max-[550px]:text-[20px] max-[430px]:text-[16px]">
                 50+            </span>
             <div className="flex flex-col gap-0">
                 <span className="text-[20px] max-[1100px]:text-[16px] max-[900px]:text-[14px] max-[550px]:text-[12px]">CERTIFIED</span>
