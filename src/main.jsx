d3e53e6bedec1c92ef6d7de4d1b6481c775ff1ff
Amaino <85074611+amainooti@users.noreply.github.com>
2024-03-23 18:10:20 +0100

    Update main.jsx
    Added something

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,19 @@ import './index.css'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Homepage from './pages/Homepage'
 import AboutUs from './pages/AboutUs'
+import NavMenu from "./components/Global/NavMenu";
+import Footer from "./components/Global/Footer";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
+      <NavMenu />
       <Routes>
         <Route path='/' element ={<Homepage />} />
         <Route path='/about-us' element ={<AboutUs />} />
       </Routes>
+      <Footer />
     </Router>
-    {/* <Homepage /> */}
+    
   </React.StrictMode>,
 )
